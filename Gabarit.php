ca387adc97bfdb722363290b61b3822d454dc51a
MarineRenard <marinerenard67@gmail.com>
2017-05-05 10:28:20 +0200

    marinegabarit

--- Gabarit.php
+++ Gabarit.php
@@ -1,8 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/base.css" />
+    <link rel="stylesheet" type="text/css" href="styles/structure.css" />
+    <title>
+        <?php echo "Accueil"; ?>
+    </title>
+</head>
+<div id="global">
+    <div id="tete">
+        <?php echo "Haut de Page"; ?>
+    </div>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: Marine
- * Date: 05/05/2017
- * Time: 09:56
- */?>
+    <hr/>
+
+    <div id="corps">
+        <div id="menu">
+            <?php echo"Menu"; ?>
+        </div>
+        <div id="contenu">
+            <?php echo "Contenu"; ?>
+        </div>
+    </div>
+
+    <hr />
+
+    <div id="pied">
+        <p>
+            PesiMod
+        </p>
+        <p>
+            <a href="_blank">Qu'est ce Pesimod ?</a> <br/>
+            <a href="_blank">Nous rejoindre</a> <br/>
+            <a href="_blank">Mentions légales</a> <br/>
+        </p>
+    </div>
+</div>
+</html>
