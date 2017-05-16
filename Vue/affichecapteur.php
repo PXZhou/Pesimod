5c67f03c550e5e1ab72cf0f4e62c068ed7b508d1
MarineRenard <marinerenard67@gmail.com>
2017-05-16 10:31:07 +0200

    marinestyle

--- Vue/affichecapteur.php
+++ Vue/affichecapteur.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marine
- * Date: 16/05/2017
- * Time: 08:59
- */
+
+    $contenu = "<h1> Affichage des capteurs </h1>";
+    $contenu .= "<ul id='listepiece' class='maliste'>";
+  //  $contenu .= "<u1>";
+    $contenu .= "<li>Chambre</li>";
+    $contenu .= "<li>Salon</li>";
+    $contenu .= "<li>Cuisine</li>";
+    $contenu .= "<li>Salle de bain</li>";
+    $contenu .= "</ul>";
+    include 'Gabarit.php';
+
+?>
