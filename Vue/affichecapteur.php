bf1251d04b09b536bfebe2356cab43a783aa3bf3
MarineRenard <marinerenard67@gmail.com>
2017-05-17 11:46:25 +0200

    marinestyle

--- Vue/affichecapteur.php
+++ Vue/affichecapteur.php
@@ -1,13 +1,20 @@
 <?php
     $title = "Affichage";
-    $contenu = "<h1> Affichage des capteurs </h1>";
-    $contenu .= "<ul id='listepiece' class='maliste'>";
+    $tete = "Affichage des capteurs";
+    $contenu = "<ul id='listepiece' class='maliste'>";
   //  $contenu .= "<u1>";
     $contenu .= "<li>Chambre</li>";
     $contenu .= "<li>Salon</li>";
     $contenu .= "<li>Cuisine</li>";
     $contenu .= "<li>Salle de bain</li>";
     $contenu .= "</ul>";
+    $contenu .= "<div id= 'contenu_Chambre'> Contenu de la chambre </div>";
+    $contenu .= "<div id= 'contenu_Salon'> Contenu du Salon </div>";
+    $contenu .= "<div id= 'contenu_Cuisine'> Contenu de la cuisine </div>";
+    $contenu .= "<div id= 'contenu_Salledebain'> Contenu de la salle de bain </div>";
+
+
+
     include 'Gabarit.php';
 
 ?>
