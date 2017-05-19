d629eb376c0dac42715f5498783422529cad247a
MarineRenard <marinerenard67@gmail.com>
2017-05-19 18:41:23 +0200

    marinestyle

--- Vue/affichecapteur.php
+++ Vue/affichecapteur.php
@@ -17,31 +17,7 @@
     $contenu .= "<div class='item'> Contenu de la salle de bain </div>";
     $contenu .= "</div>";
 
-    $jvscrpt .= "<script src=\"http://code.jquery.com/jquery-latest.js\"></script>
-    <script>
-    $(function() {
-        $('#listepiece').css('display', 'block');
-        $('#listepiece').click(function(event) {
-            var actuel = event.target;
-            if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
-                alert(actuel.nodeName)
-                return;
-            }
-            $(actuel).addClass('actif').siblings().removeClass('actif');
-            setDisplay();
-        });
-        function setDisplay() {
-            var modeAffichage;
-            $('listepiece li').each(function(rang) {
-                modeAffichage = $(this).hasClass('actif') ? '' : 'none';
-                $('.item').eq(rang).css('display', modeAffichage);
-            });
-        }
-        setDisplay();
-    });
-</script>";
-
-
+    $jvscrpt = require('Vue/onglets.php');
 
 
     include 'Gabarit.php';
