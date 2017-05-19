1ab6dddab140028e5118ea8237756f63c5d2e687
MarineRenard <marinerenard67@gmail.com>
2017-05-19 18:41:55 +0200

    marinestyle

--- Vue/onglets.php
+++ Vue/onglets.php
@@ -1,26 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <script src=\"http://code.jquery.com/jquery-latest.js\"></script>
-    <script>
-        $(function() {
-            $('#listepiece').css('display', 'block');
-            $('#listepiece').click(function(event) {
-                var actuel = event.target;
-                if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
-                    alert(actuel.nodeName)
-                    return;
-                }
-                $(actuel).addClass('actif').siblings().removeClass('actif');
-                setDisplay();
-            });
-            function setDisplay() {
-                var modeAffichage;
-                $('listepiece li').each(function(rang) {
-                    modeAffichage = $(this).hasClass('actif') ? '' : 'none';
-                    $('.item').eq(rang).css('display', modeAffichage);
-                });
-            }
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+    $(function() {
+        $('#listepiece').css('display', 'block'); //Afficher la barre d'onglet
+        $('#listepiece').click(function(event) { //Evenement clique
+            var actuel = event.target; // emplacement du clique
+            if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
+                alert(actuel.nodeName)
+                return;
+            } //Si pas onglet ou si onglet deja actif, ne rien faire
+            $(actuel).addClass('actif').siblings().removeClass('actif');
             setDisplay();
         });
-    </script>";
-</html>
+        function setDisplay() {
+            var modeAffichage;
+            $('#listepiece li').each(function(rang) {
+                modeAffichage = $(this).hasClass('actif') ? '' : 'none';
+                $('.item').eq(rang).css('display', modeAffichage);
+            });
+        }
+        setDisplay();
+    })
+</script>
