4af33b8387f626c9d379c1a7a02e700dcf43310f
Thoreuxyohan <yohan.thoreux972@gmail.com>
2017-05-19 11:53:18 +0200

    test 13

---
+++ Modele/connexion.php
@@ -0,0 +1,10 @@
+<?php
+$dbname = "APP-MVC";
+$host='localhost';
+$user='root';
+$pass='';
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
+$db->query("SET NAMES UTF8");
+
+?>
