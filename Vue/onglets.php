edba116ddde7146067dafa3c58d748966b1a5262
MarineRenard <marinerenard67@gmail.com>
2017-05-19 14:10:35 +0200

    marinestyle

--- Vue/onglets.php
+++ Vue/onglets.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marine
- * Date: 19/05/2017
- * Time: 14:08
- */
+<!DOCTYPE html>
+<html>
+    <script src=\"http://code.jquery.com/jquery-latest.js\"></script>
+    <script>
+        $(function() {
+            $('#listepiece').css('display', 'block');
+            $('#listepiece').click(function(event) {
+                var actuel = event.target;
+                if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
+                    alert(actuel.nodeName)
+                    return;
+                }
+                $(actuel).addClass('actif').siblings().removeClass('actif');
+                setDisplay();
+            });
+            function setDisplay() {
+                var modeAffichage;
+                $('listepiece li').each(function(rang) {
+                    modeAffichage = $(this).hasClass('actif') ? '' : 'none';
+                    $('.item').eq(rang).css('display', modeAffichage);
+                });
+            }
+            setDisplay();
+        });
+    </script>";
+</html>
