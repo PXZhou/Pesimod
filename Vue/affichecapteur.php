e3030a9af0823a0106cab4bc722afe0377c01fcf
MarineRenard <marinerenard67@gmail.com>
2017-05-19 13:41:24 +0200

    marinestyle

--- Vue/affichecapteur.php
+++ Vue/affichecapteur.php
@@ -4,33 +4,43 @@
 
 
 
-    $contenu = "<ul id='listepiece' class='maliste'>";
-    //  $contenu .= "<u1>";
-    $contenu .= "<li><a href='#Chambre1' divToShow='contenu_Chambre1'>Chambre</a></li>";
-    $contenu .= "<li><a href='#Salon1' divToShow='contenu_Salon1'>Salon</a></li>";
-    $contenu .= "<li><a href='#Cuisine1'  divToShow='contenu_Cuisine1'>Cuisine</a></li>";
-    $contenu .= "<li><a href='#Salledebain1' divToShow='contenu_Salledebain1'>Salle de bain</a></li>";
+    $contenu = "<ul id='listepiece'>";
+    $contenu .= "<li class='actif'>Chambre</li>";
+    $contenu .= "<li>Salon</li>";
+    $contenu .= "<li>Cuisine</li>";
+    $contenu .= "<li>Salle de bain</li>";
     $contenu .= "</ul>";
     $contenu .= "<div id='contentonglet'> ";
-    $contenu .= "<div id= 'contenu_Chambre1' class='show'> Contenu de la Chambre 1 </div>";
-    $contenu .= "<div id= 'contenu_Salon1' style='display:none;'> Contenu du Salon </div>";
-    $contenu .= "<div id= 'contenu_Cuisine1' style='display:none;'> Contenu de la cuisine </div>";
-    $contenu .= "<div id= 'contenu_Salledebain1' style='display:none;'> Contenu de la salle de bain </div>";
+    $contenu .= "<div class='item'> Contenu de la Chambre</div>";
+    $contenu .= "<div class='item'> Contenu du Salon </div>";
+    $contenu .= "<div class='item'> Contenu de la cuisine </div>";
+    $contenu .= "<div class='item'> Contenu de la salle de bain </div>";
     $contenu .= "</div>";
 
-   $contenu .="
-    <script type=\"text/javascript\"
-            src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js\"></script>
-    <script type=\"text/javascript\">
-        $('#menu a' ).click(function() {
-        elementToFadeIn = $(this).attr('divToShow');
-  	    $('.show').fadeOut(function(){
-            $(this).removeClass('show');
-            $('#'+elementToFadeIn).fadeIn(function(){
-                 $(this).addClass('show');
-          });
-      });
-    });</script>";
+   $jvscrpt ="
+	<script src=\"http://code.jquery.com/jquery-latest.js\"></script>
+	<script>
+		$(function() {
+			$('#listepiece').css('display', 'block');
+			$('#listepiece').click(function(event) {
+				var actuel = event.target;
+				if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
+					alert(actuel.nodeName)
+					return;
+				}
+				$(actuel).addClass('actif').siblings().removeClass('actif');
+				setDisplay();
+			});
+			function setDisplay() {
+				var modeAffichage;
+				$('listepiece li').each(function(rang) {
+					modeAffichage = $(this).hasClass('actif') ? '' : 'none';
+					$('.item').eq(rang).css('display', modeAffichage);
+				});
+			}
+			setDisplay();
+		});
+	</script>";
 
 
 
