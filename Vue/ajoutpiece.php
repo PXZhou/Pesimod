80f96061bd9e84abf1355b877957f37c032f80b3
MarineRenard <marinerenard67@gmail.com>
2017-05-21 16:36:34 +0200

    marinestyle

--- Vue/ajoutpiece.php
+++ Vue/ajoutpiece.php
@@ -1,1 +1,29 @@
+<ul id='addpiece'>
+    <li>Nom de la pièce : </li> <input type='text' name='nom_piece' />
+    <li>Type de pièce :</li>
+    </ul>
+
+<form method="post" action="list_detail.php">
+    <select name="typepiece">
+
+        <option name="Cuisine">cuisine</option>
+        <option name="SalledeBain">Salle de Bain</option>
+        <option name="Salon">Salon</option>
+        <option name="Chambre">Chambre</option>
+        <option name="Pieceavivre">Pièce à vivre</option>
+        <option name="Cagibi">Cagibi</option>
+        <input type="submit" name="Valider" value="OK1"/>
+
+    </select>
+
+</form>
+
 <?php
+//On traite le formulaire
+if(isset($_POST['champ'])){
+    echo ' variable projet : ' ;
+    $tt= $_POST['champ'];
+    echo $tt;
+
+}
+?>
