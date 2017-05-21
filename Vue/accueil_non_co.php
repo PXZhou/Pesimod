969ad7ba4540ef37291e021937f614f3d0205fc0
MarineRenard <marinerenard67@gmail.com>
2017-05-21 20:06:16 +0200

    marinestyle

--- Vue/accueil_non_co.php
+++ Vue/accueil_non_co.php
@@ -2,6 +2,7 @@
     $title = "Accueil";
     $tete = "Accueil";
     $contenu = "Accueuil. Veuillez vous connecter";
+    $contenu .= "<p><a href='index.php?cible=hacker'>Bruce</a></p>";
 
     $menu_der="";
     $jvscript = "";
