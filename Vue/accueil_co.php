351ea74b157d9f9c7c94bc4229ec874be55e3a57
MarineRenard <marinerenard67@gmail.com>
2017-05-21 18:34:22 +0200

    marinestyle

--- Vue/accueil_co.php
+++ Vue/accueil_co.php
@@ -1,6 +1,7 @@
 <?php
-
-    $contenu = "Accueil";
+    $tete = "La tête";
+    $contenu = "<p>Accueil</p>
+    <p><a href='Vue/affichecapteur.php'><img src='Style/Image/001-facebook.png'></a></p>";
     $jvscript = "";
 
     include 'Gabarit.php';
