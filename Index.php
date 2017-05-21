841c92a3b6491eebfb850237e60fe3240df9efb1
MarineRenard <marinerenard67@gmail.com>
2017-05-21 19:29:16 +0200

    marinestyle

--- Index.php
+++ Index.php
@@ -9,22 +9,14 @@
     include("Vue/accueil_co.php");*/
 
 
-    if(!isset($_SESSION["userID"])){
-        //include ("Vue/accueil_non_co.php");
-        include ("Vue/accueil_co.php");
+    if(!isset($_SESSION["userID"])){ //Utilisateur pas connecté
+        include ("Vue/accueil_non_co.php");
         if(isset($_GET['cible'])) {
             if ($_GET['cible'] == 'se_connecter') {
-                ob_clean();
-                include("Controler/Connexion.php");
+                include("Controler/Connexion.php"); // On utilise un controleur secondaire pour éviter d'avoir un fichier index.php trop gros
             } else if ($_GET['cible'] == 's_inscrire') {
                 ob_clean();
                 include("Controler/inscription.php");
-            } else if ($_GET['cible'] == 'affichecapteur'){
-                ob_clean();
-                include("Vue/affichecapteur.php");
-            } else if ($_GET['cible'] == 'accueil') {
-                ob_clean();
-                include("Vue/accueil.php");
             }
         }
     }
