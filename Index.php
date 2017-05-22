473517d547a8846f9e98cb4d1084f603d3cbebde
MarineRenard <marinerenard67@gmail.com>
2017-05-22 09:38:35 +0200

    marinestyle

--- Index.php
+++ Index.php
@@ -7,26 +7,31 @@
     //require("Modele/connexion.php");
     /*$_SESSION["userID"] = $ligne['id'];
     include("Vue/accueil_co.php");*/
-require("Vue/accueil_co.php");
-session_start();
+require("Vue/accueil_non_co.php");
+
 
     if(!isset($_SESSION["userID"])){
         //include ("Vue/accueil_non_co.php");
-        include ("Vue/accueil_co.php");
+        ob_clean();
+        include ("Vue/accueil_non_co.php");
         if(isset($_GET['cible'])) {
             if ($_GET['cible'] == 'se_connecter') {
+                ob_clean();
                 include("Controler/Connexion.php");
             } else if ($_GET['cible'] == 's_inscrire') {
+                ob_clean();
                 include("Controler/inscription.php");
             } else if ($_GET['cible'] == 'affichecapteur'){
+                ob_clean();
                 include("Vue/affichecapteur.php");
             } else if ($_GET['cible'] == 'accueil') {
+                ob_clean();
                 include("Vue/accueil.php");
             }
             if ($_GET['cible']=='hacker') {
                 ob_clean();
                 $_SESSION["userID"] = $ligne['3'];
-                include("Vue/affichecapteur.php");
+                include("Vue/accueil_co.php");
             }
 
         }
