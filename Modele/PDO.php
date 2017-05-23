bcd2684f07ff00ab53bbde2ba2afa9bd1b0fd8c7
Thoreuxyohan <yohan.thoreux972@gmail.com>
2017-05-23 09:50:18 +0200

    Modification fichiers

--- Modele/PDO.php
+++ Modele/PDO.php
@@ -8,7 +8,7 @@
 
     // fonction qui cherche le mot de passe d'un utilisateur avec un identifiant dans la base de données
     function utilisateurs($db){
-        $reponse = $db->query('SELECT nom FROM utilisateur');
+        $reponse = $db->query('SELECT Nom FROM utilisateur');
         return $reponse;
     }
 ?>
