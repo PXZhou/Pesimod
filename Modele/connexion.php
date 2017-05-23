16754446aa67f0c19589305d401d3749e659e30f
alexpikx <a.larrive@hotmail.com>
2017-05-23 09:48:58 +0200

    Site dynamique :)

--- Modele/connexion.php
+++ Modele/connexion.php
@@ -2,7 +2,7 @@
 $dbname = "espace_clientb";
 $host='localhost';
 $user='root';
-$pass='';
+$pass='root';
 
 $db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
 $db->query("SET NAMES UTF8");
