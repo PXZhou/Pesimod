2c8f528b1d44fa2a022d176f454789c59237f34a
MarineRenard <marinerenard67@gmail.com>
2017-06-02 11:01:38 +0200

    marinestyle

--- Vue/affichecapteur.php
+++ Vue/affichecapteur.php
@@ -2,8 +2,7 @@
 <?php
 $title = "Affichage";
 $tete = "Affichage des capteurs";
-$dbouton1 = dbouton1("");
-$dbouton2 = dbouton2("Se déconnecter");
+
 
 
 $contenu = "<ul id='onglet'>";
@@ -17,22 +16,45 @@ $contenu .= "</ul>";
 $contenu .= "<div id='contentonglet'> ";
 $contenu .= "<div class='item'> <h6>Capteur température</h6>
 <p><img src='Style/Image/temperature.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+<input id='echelle' type='range'>
 <h6>Capteur luminosité</h6>
 <p><img src='Style/Image/luminosite.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+
+
+
+<input class='switch' name='premium' id='premium' type='checkbox' checked>
+<label for='premium' class='ui-content' >
+  <div class='ui-switch-range'>
+     <div class='ui-switch-handle'>
+     </div>
+  </div>
+</label>
+
+
+
 <h6>Capteur humidité</h6>
 <p><img src='Style/Image/fumee.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+
+
+<input class='switch' name='premium1' id='premium' type='checkbox' checked>
+<label for='premium1' class='ui-content' >
+  <div class='ui-switch-range'>
+     <div class='ui-switch-handle'>
+     </div>
+  </div>
+</label>
+
+
+
 <h6>Capteur fumée</h6>
 <p><img src='Style/Image/fumee.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+<input type='checkbox' checked data-toggle='toggle'>
    <p><a href='#'><img src='Style/Image/Poubelle.png' alt='Suppression' width='40' height='40' ></a></p></div>";
 
 //----------------------------------------------------------------
 $contenu .= "<div class='item'> <h6>Capteur température</h6>
 <p><img src='Style/Image/temperature.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+<input id='echelle' type='range'>
 <h6>Capteur luminosité 1</h6>
 <p><img src='Style/Image/luminosite.png' alt='temp' width='40' height='40' ></p>
 <img src='Style/Image/on.png' alt='switch' width='35' height='35'>
@@ -47,7 +69,7 @@ $contenu .= "<div class='item'> <h6>Capteur température</h6>
 //----------------------------------------------------------------
 $contenu .= "<div class='item'> <h6>Capteur température</h6>
 <p><img src='Style/Image/temperature.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+<input id='echelle' type='range'>
 <h6>Capteur luminosité</h6>
 <p><img src='Style/Image/luminosite.png' alt='temp' width='40' height='40' ></p>
 <img src='Style/Image/on.png' alt='switch' width='35' height='35'>
@@ -56,7 +78,7 @@ $contenu .= "<div class='item'> <h6>Capteur température</h6>
 //----------------------------------------------------------------
 $contenu .= "<div class='item'> <h6>Capteur température</h6>
 <p><img src='Style/Image/temperature.png' alt='temp' width='40' height='40' ></p>
-<img src='Style/Image/on.png' alt='switch' width='35' height='35'>
+<input id='echelle' type='range'>
 <h6>Capteur luminosité 1</h6>
 <p><img src='Style/Image/luminosite.png' alt='temp' width='40' height='40' ></p>
 <img src='Style/Image/on.png' alt='switch' width='35' height='35'>
@@ -74,15 +96,6 @@ $contenu .= "<div class='item'>";
 $contenu .= "
 <form method='post' action='list_detail.php'>
    <p>Nom de la pièce : <input type='text' name='nom_piece' /></p>
-   <p>Type de pièce :
-   <select name='typepiece'>
-       <option name='Cuisine'>Cuisine</option>
-       <option name='SalledeBain'>Salle de Bain</option>
-       <option name='Salon'>Salon</option>
-       <option name='Chambre'>Chambre</option>
-       <option name='Pieceavivre'>Pièce à vivre</option>
-       <option name='Cagibi'>Cagibi</option>
-   </select></p>
    <p><input type='submit' value='Valider'/>
    <input type='reset' value='Annuler' /></p>
 
@@ -90,6 +103,7 @@ $contenu .= "
 $contenu .= "</div>";
 ob_clean();
 $jvscrpt = require('Vue/onglets.php');
+
 include 'Gabarit.php';
 
 ?>
