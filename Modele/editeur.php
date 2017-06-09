940b9d21f2a47a0cb58b727842e19f7499d3070f
PXZhou <packrizouh@gmail.com>
2017-06-09 10:26:00 +0200

    Travail avec le fichier sql espace_clientB

--- Modele/editeur.php
+++ Modele/editeur.php
@@ -3,6 +3,7 @@
         $req = $bdd->exec("SELECT id FROM '.$nom.'");
         return $req;
     }
+
     function get_info($id,$bdd){
         $req = $bdd->exec("SELECT maison.etage,piece.id FROM utilisateur JOIN maison on utilisateur.id = maison.id_utilisateur 
 JOIN piece on maison.id = piece.id_maison where utilisateur.id = '.$id.'");
