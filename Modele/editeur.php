4c66a8a8904fc8887acf5957829fc5a1f790df5a
PXZhou <packrizouh@gmail.com>
2017-06-09 10:19:18 +0200

    en attente

---
+++ Modele/editeur.php
@@ -0,0 +1,10 @@
+<?php
+    function get_id($nom,$bdd){
+        $req = $bdd->exec("SELECT id FROM '.$nom.'");
+        return $req;
+    }
+    function get_info($id,$bdd){
+        $req = $bdd->exec("SELECT maison.etage,piece.id FROM utilisateur JOIN maison on utilisateur.id = maison.id_utilisateur 
+JOIN piece on maison.id = piece.id_maison where utilisateur.id = '.$id.'");
+        return $req;
+    }
