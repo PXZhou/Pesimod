84e66e51dff9799a3d7a394849c77afe43c9dfa9
ElisaPTK <elisa.ptak@isep.fr>
2017-06-19 09:34:14 +0200

    CGU

--- Vue/Commun.php
+++ Vue/Commun.php
@@ -52,12 +52,12 @@ function formulaire_ins(){
             <br/>
             <input type="text" name="Mdp"/>
             <br/>
-            Confirmation "Mot de passe
+            Confirmation "Mot de passe"
             <br/>
             <input type="text" name="Mdp2"/>
 
             <br/>
-            <p><a href="CGU">Conditions generales d'utilisation </a><input type="checkbox" name="macase" value="Conditions generales d'utilisation">;</p>
+            <p><a href="CGU">Conditions générales d'utilisation </a><input type="checkbox" name="macase" value="Conditions generales d'utilisation"></p>
 
             <input type='submit' value="Valider"/>
             </div>
